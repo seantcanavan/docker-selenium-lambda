4329093715af7968a807f227e81826c2be61e538
seantcanavan@gmail.com <seantcanavan@gmail.com>
2024-01-30 16:21:01 -0600

    WIP... trying to figure out pyenv...
    Signed-off-by: seantcanavan@gmail.com <seantcanavan@gmail.com>

--- Makefile
+++ Makefile
@@ -1,13 +1,24 @@
-create:
-	sls create --template-url "https://github.com/umihico/docker-selenium-lambda/tree/main" --path docker-selenium-lambda && cd $_
+SHELL := /bin/bash
 
 deploy-staging:
-	serverless deploy --verbose --stage staging --region us-east-2 --org f72e1c13062e4f45ad951530acf9e5a7
+	serverless deploy --verbose --force --stage staging --region us-east-2 --org f72e1c13062e4f45ad951530acf9e5a7
 
 deps:
-	sudo pacman -Syu docker
+	@. ~/.nvm/nvm.sh; nvm use; node --version
+	sudo pacman -Syu --needed docker pyenv
 	sudo systemctl start docker.service
 	sudo systemctl enable docker.service
+	npm install -g serverless
+
+python:
+	pyenv install 3.12.0
+	python3 -m venv venv
+
+pipenv:
+	#python3 -m ensurepip --upgrade
+	pip3 install --upgrade pip
+	pip3 install pipenv
+	pipenv --python 3.12
 
 invoke-staging:
 	serverless invoke --stage staging --region us-east-2 --function demo
--- main.py
+++ main.py
@@ -1,27 +1,44 @@
-from selenium import webdriver
+import os
+import sys
+from os import environ
 from tempfile import mkdtemp
+
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+options = webdriver.ChromeOptions()
+service = webdriver.ChromeService("/opt/chromedriver")
 
+options.binary_location = '/opt/chrome/chrome'
+options.add_argument("--headless=new")
+options.add_argument('--no-sandbox')
+options.add_argument("--disable-gpu")
+options.add_argument("--window-size=1280x1696")
+options.add_argument("--single-process")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-dev-tools")
+options.add_argument("--no-zygote")
+options.add_argument(f"--user-data-dir={mkdtemp()}")
+options.add_argument(f"--data-path={mkdtemp()}")
+options.add_argument(f"--disk-cache-dir={mkdtemp()}")
+options.add_argument("--remote-debugging-port=9222")
+chrome = webdriver.Chrome(options=options, service=service)
+
+
+# lambda handler entry point
 def handler(event=None, context=None):
-    options = webdriver.ChromeOptions()
-    service = webdriver.ChromeService("/opt/chromedriver")
+    print("Python version")
+    print(sys.version)
+    print("Version info.")
+    print(sys.version_info)
 
-    options.binary_location = '/opt/chrome/chrome'
-    options.add_argument("--headless=new")
-    options.add_argument('--no-sandbox')
-    options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1280x1696")
-    options.add_argument("--single-process")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-dev-tools")
-    options.add_argument("--no-zygote")
-    options.add_argument(f"--user-data-dir={mkdtemp()}")
-    options.add_argument(f"--data-path={mkdtemp()}")
-    options.add_argument(f"--disk-cache-dir={mkdtemp()}")
-    options.add_argument("--remote-debugging-port=9222")
+    for key, value in os.environ.items():
+        print(f"{key}: {value}")
 
-    chrome = webdriver.Chrome(options=options, service=service)
     chrome.get("https://example.com/")
 
-    return chrome.find_element(by=By.XPATH, value="//html").text
+    return environ.get("TEST_ENV") + "HITHERE" + chrome.find_element(by=By.XPATH, value="//html").text
+
+
+def local_handler(event=None, context=None):
+    print("hi local python")
